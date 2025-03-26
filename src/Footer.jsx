a0ab70f598a62eecf1ce7a026ffde8ba88ebd99f
Sujal Jain <63149580+Sujal942@users.noreply.github.com>
2025-03-26 15:09:14 +0530

    Ai Solver

---
+++ src/Footer.jsx
@@ -0,0 +1,23 @@
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-center text-white mt-5">
+      {/* Grid container */}
+      <div className="container p-4"></div>
+
+      {/* Section: Text */}
+      <section className="mb-4">
+        <p>Designed & Developed by SS</p>
+      </section>
+
+      {/* Copyright */}
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        © 2025 Copyright: AI Solver
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
