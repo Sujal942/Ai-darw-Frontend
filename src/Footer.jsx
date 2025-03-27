24042a3132ca2edcc54dc001796bd5b6282cc2d0
Sujal Jain <63149580+Sujal942@users.noreply.github.com>
2025-03-27 13:28:31 +0530

    fix footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
         className="text-center p-3"
         style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
       >
-        © 2025 Copyright: AI Solver
+        © 2025 Copyright: AI Draw
       </div>
     </footer>
   );
